60f1e4cd70a6216c9f8636af6a52dd7b92fb9387
Demezy <38487319+Demezy@users.noreply.github.com>
2023-06-15 14:38:43 +0300

    feat: implement task priority to human text extension

---
+++ lib/task_edit/utils/task_priority_to_human_string_extension.dart
@@ -0,0 +1,16 @@
+import 'package:todo_app/tasks_overview/domain/task_entry.dart';
+
+extension TaskPriorityToHumanStringExtension on TaskPriority {
+  String humanString() {
+    switch (this) {
+      case TaskPriority.low:
+        return 'Низкий';
+      case TaskPriority.medium:
+        return 'Средний';
+      case TaskPriority.high:
+        return '!! Высокий';
+      case TaskPriority.none:
+        return 'Нет';
+    }
+  }
+}
