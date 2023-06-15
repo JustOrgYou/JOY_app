825b9391f229190bf7e38ef64d33d87cc28b9a17
Demezy <38487319+Demezy@users.noreply.github.com>
2023-06-15 14:37:07 +0300

    feat: implement task priority to color extension

---
+++ lib/task_edit/utils/task_priority_to_color_extension.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/tasks_overview/domain/task_entry.dart';
+
+extension TaskPriorityToColorExtension on TaskPriority {
+  Color color(BuildContext context) {
+    switch (this) {
+      case TaskPriority.none:
+        return Colors.grey;
+      case TaskPriority.high:
+        return Colors.red;
+      default:
+        return Theme.of(context).textTheme.bodyMedium?.color ?? Colors.black;
+    }
+  }
+}
