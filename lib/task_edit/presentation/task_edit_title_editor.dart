776a0781e63c8902c1de698d866f0a433fba4531
Demezy <38487319+Demezy@users.noreply.github.com>
2023-06-15 14:34:43 +0300

    feat: implement basic task edit text field

---
+++ lib/task_edit/presentation/task_edit_title_editor.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class TaskEditTitleEditor extends StatelessWidget {
+  final TextEditingController textEditingController;
+  const TaskEditTitleEditor({
+    required this.textEditingController,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      maxLines: null,
+      minLines: 3,
+      controller: textEditingController,
+      style: Theme.of(context).textTheme.bodyMedium,
+      decoration: InputDecoration(
+        hintText: 'Что надо сделать...',
+        border: InputBorder.none,
+        fillColor: Theme.of(context).colorScheme.background,
+        filled: true,
+        contentPadding: const EdgeInsets.all(12),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(
+            color: Theme.of(context).colorScheme.background,
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(
+            color: Theme.of(context).colorScheme.background,
+          ),
+        ),
+      ),
+    );
+  }
+}
