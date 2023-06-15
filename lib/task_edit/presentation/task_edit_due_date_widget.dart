59a69c888d4e980446fd8d025f4dd99cb34e65b1
Demezy <38487319+Demezy@users.noreply.github.com>
2023-06-15 14:32:26 +0300

    feat: implement task edit date picker

---
+++ lib/task_edit/presentation/task_edit_due_date_widget.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/task_edit/utils/date_time_formatted_extension.dart';
+
+class TaskEditDueDateWidget extends StatelessWidget {
+  final ValueNotifier<DateTime?> dueDate;
+  const TaskEditDueDateWidget({
+    required this.dueDate,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            /// title
+            Text(
+              'Сделать до',
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+
+            /// toggle
+            Switch(
+              activeColor: Theme.of(context).colorScheme.primary,
+              value: dueDate.value != null,
+              onChanged: (dueDateActive) async {
+                if (dueDateActive) {
+                  final now = DateTime.now();
+                  final pickedDate = await showDatePicker(
+                    context: context,
+                    initialDate: now,
+                    firstDate: now.subtract(
+                      const Duration(days: 3650),
+                    ),
+                    lastDate: now.add(
+                      const Duration(days: 3650),
+                    ),
+                  );
+                  dueDate.value = pickedDate;
+                } else {
+                  dueDate.value = null;
+                }
+              },
+            ),
+          ],
+        ),
+
+        /// date display
+        if (dueDate.value != null)
+          Text(
+            dueDate.value!.formatted(),
+            style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+          ),
+      ],
+    );
+  }
+}
