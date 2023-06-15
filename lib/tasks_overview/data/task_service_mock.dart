a9092e70ad44fc455d827ac60f0b93c9882f2b9f
Demezy <38487319+Demezy@users.noreply.github.com>
2023-06-15 14:24:03 +0300

    feat: make mock task service maintain state

--- lib/tasks_overview/data/task_service_mock.dart
+++ lib/tasks_overview/data/task_service_mock.dart
@@ -1,42 +1,59 @@
+import 'dart:async';
+
 import 'package:todo_app/tasks_overview/domain/task_entry.dart';
 import 'package:todo_app/tasks_overview/domain/task_service.dart';
 
 class TaskEntryServiceMock implements TaskEntryService {
+  final _taskEntries = List<TaskEntry>.generate(
+    100,
+    (index) => TaskEntry(
+      id: index,
+      title: 'Task $index',
+      priority: TaskPriority.values[index % TaskPriority.values.length],
+      status: TaskStatus.values[index % TaskStatus.values.length],
+      dueDate: DateTime.now().add(Duration(days: index)),
+    ),
+  );
+  final _taskEntriesStreamController = StreamController<List<TaskEntry>>();
+
+  TaskEntryServiceMock() {
+    _taskEntriesStreamController.add(_taskEntries);
+  }
+
+  @override
   Future<List<TaskEntry>> getTaskEntries() async {
     await Future.delayed(
       const Duration(seconds: 2),
     );
-    return List<TaskEntry>.generate(
-      100,
-      (index) => TaskEntry(
-        id: index,
-        title: 'Task $index',
-        priority: TaskPriority.values[index % TaskPriority.values.length],
-        status: TaskStatus.values[index % TaskStatus.values.length],
-        dueDate: DateTime.now().add(Duration(days: index)),
-      ),
-    );
+    return _taskEntries;
   }
 
   Future<void> addTaskEntry(TaskEntry taskEntry) async {
     await Future.delayed(
       const Duration(seconds: 2),
     );
-  }
-
-  Future<void> updateTaskEntry(TaskEntry taskEntry) async {
-    await Future.delayed(const Duration(seconds: 2));
+    _taskEntries.add(taskEntry);
+    _taskEntriesStreamController.add(_taskEntries);
   }
 
   @override
-  Future<void> createTaskEntry(TaskEntry taskEntry) async {
-    // mock does not has state
-    await Future.delayed(const Duration(seconds: 1));
+  Future<void> updateTaskEntry(TaskEntry taskEntry) async {
+    await Future.delayed(const Duration(seconds: 2));
+    final taskEntryIndex = _taskEntries.indexWhere(
+      (entry) => entry.id == taskEntry.id,
+    );
+    _taskEntries[taskEntryIndex] = taskEntry;
   }
 
   @override
   Future<void> deleteTaskEntry(int id) async {
-    // mock does not has state
     await Future.delayed(const Duration(seconds: 1));
+    _taskEntries.removeWhere((taskEntry) => taskEntry.id == id);
+    _taskEntriesStreamController.add(_taskEntries);
+  }
+
+  @override
+  Stream<List<TaskEntry>> getTaskEntriesStream() {
+    return _taskEntriesStreamController.stream;
   }
 }
