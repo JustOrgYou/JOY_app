731f97a59320a4c901a29e440de6a691d4a1c5e6
Demezy <38487319+Demezy@users.noreply.github.com>
2023-06-24 17:23:32 +0300

    feat: setup isar with sample data

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo_app/config/app_theme.dart';
+import 'package:todo_app/local_storage/data/local_storage_isar.dart';
+import 'package:todo_app/local_storage/data/local_storage_provider.dart';
 import 'package:todo_app/tasks_overview/tasks_overview.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<List<Override>> setup() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  /// initialize local storage
+  final localStorage = LocalStorageIsar();
+  await localStorage.initialize();
+  return [
+    localStorageProvider.overrideWithValue(localStorage),
+  ];
+}
+
+void main() async {
+  runApp(
+    MyApp(
+      overrides: await setup(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final List<Override> overrides;
+  const MyApp({
+    required this.overrides,
+    super.key,
+  });
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return ProviderScope(
+      overrides: overrides,
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: AppTheme.lightTheme,
