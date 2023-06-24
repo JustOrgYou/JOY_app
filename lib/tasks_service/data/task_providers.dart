c43a2f01b5a17f9a1f923650ebb271c27d70dc5c
Demezy <38487319+Demezy@users.noreply.github.com>
2023-06-24 20:27:44 +0300

    feat: DI requirements in providers

--- lib/tasks_service/data/task_providers.dart
+++ lib/tasks_service/data/task_providers.dart
@@ -1,13 +1,20 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:todo_app/tasks_service/data/task_service_mock.dart';
+import 'package:todo_app/local_storage/data/local_storage_provider.dart';
+import 'package:todo_app/tasks_service/data/task_service_local_remote.dart';
 import 'package:todo_app/tasks_service/domain/task_entry.dart';
 import 'package:todo_app/tasks_service/domain/task_service.dart';
 
 final taskEntryServiceProvider = Provider<TaskEntryService>(
-  (ref) => TaskEntryServiceMock(),
+  (ref) {
+    final local = ref.watch(localStorageProvider);
+    return TaskEntryServiceLocalRemote(local: local);
+  },
 );
 
 final taskEntryStreamProvider = StreamProvider.autoDispose<List<TaskEntry>>(
-  (ref) => ref.watch(taskEntryServiceProvider).getTaskEntriesStream(),
+  (ref) {
+    return ref.watch(taskEntryServiceProvider).getTaskEntriesStream();
+  },
 );
+
 final doneTasksVisibilityProvider = StateProvider<bool>((ref) => true);
