495225b48784d7e55639a8e96a10da606aaa13a7
Demezy <38487319+Demezy@users.noreply.github.com>
2023-06-25 03:12:08 +0300

    feat: integrate network to task service

--- lib/tasks_service/data/task_providers.dart
+++ lib/tasks_service/data/task_providers.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo_app/local_storage/data/local_storage_provider.dart';
+import 'package:todo_app/network/data/network_providers.dart';
 import 'package:todo_app/tasks_service/data/task_service_local_remote.dart';
 import 'package:todo_app/tasks_service/domain/task_entry.dart';
 import 'package:todo_app/tasks_service/domain/task_service.dart';
@@ -7,7 +8,8 @@ import 'package:todo_app/tasks_service/domain/task_service.dart';
 final taskEntryServiceProvider = Provider<TaskEntryService>(
   (ref) {
     final local = ref.watch(localStorageProvider);
-    return TaskEntryServiceLocalRemote(local: local);
+    final remote = ref.watch(networkTasksRepositoryProvider);
+    return TaskEntryServiceLocalRemote(local: local, remote: remote);
   },
 );
 
--- lib/tasks_service/data/task_service_local_remote.dart
+++ lib/tasks_service/data/task_service_local_remote.dart
@@ -1,22 +1,31 @@
 import 'package:todo_app/local_storage/domain/local_storage.dart';
+import 'package:todo_app/network/domain/network_repository.dart';
 import 'package:todo_app/tasks_service/domain/task_entry.dart';
 import 'package:todo_app/tasks_service/domain/task_service.dart';
 
 class TaskEntryServiceLocalRemote implements TaskEntryService {
+  /// repository that is used for persistent app state management
   final LocalStorage local;
 
+  /// remote server api that sync data across devices
+  final NetworkRepository<TaskEntry> remote;
+
   TaskEntryServiceLocalRemote({
+    required this.remote,
     required this.local,
     // required this.remote,
   });
+
   @override
   Future<void> addTaskEntry(TaskEntry taskEntry) async {
     await local.tasksRepository().createOne(taskEntry);
+    await remote.createOne(taskEntry);
   }
 
   @override
   Future<void> deleteTaskEntry(TaskEntry taskEntry) async {
     await local.tasksRepository().deleteOne(taskEntry);
+    await remote.deleteOne(taskEntry);
   }
 
   @override
@@ -32,5 +41,22 @@ class TaskEntryServiceLocalRemote implements TaskEntryService {
   @override
   Future<void> updateTaskEntry(TaskEntry taskEntry) async {
     await local.tasksRepository().updateOne(taskEntry);
+    await remote.updateOne(taskEntry);
+  }
+
+  @override
+  Future<void> deleteAllTaskEntries() async {
+    await local.tasksRepository().clearItems();
+    await remote.clearItems();
+  }
+
+  /// currently syncronization is server responsibility and remote dominate over local.
+  @override
+  Future<void> syncronizeTaskEntries() async {
+    final localTaskEntries = await local.tasksRepository().getAllItems();
+    await remote.patch(localTaskEntries);
+    final remoteItems = await remote.getAllItems();
+    await local.tasksRepository().clearItems();
+    await local.tasksRepository().createMany(remoteItems);
   }
 }
