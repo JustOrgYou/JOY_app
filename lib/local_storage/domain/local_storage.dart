d9dd9334eb549b9290a5f337da0c17b0fdcee48c
Demezy <38487319+Demezy@users.noreply.github.com>
2023-06-25 03:13:03 +0300

    docs: add comments

--- lib/local_storage/domain/local_storage.dart
+++ lib/local_storage/domain/local_storage.dart
@@ -1,6 +1,8 @@
 import 'package:todo_app/local_storage/domain/local_storage_repository.dart';
 import 'package:todo_app/tasks_service/domain/task_entry.dart';
 
+/// Local Storage is responsible for persistent app state management. To separete logic, independend
+/// parts are realized through repositories.
 abstract class LocalStorage {
   /// call initialize() before using any other methods.
   /// In general is recommended to do it once and before app run.
--- lib/network/domain/network_repository.dart
+++ lib/network/domain/network_repository.dart
@@ -1,5 +1,7 @@
 import 'package:todo_app/shared/domain/crud_repository.dart';
 
+/// Network Repository is responsible for remote data management. It store no state except cached revision.
+/// Primary purpose is to wrap network logic and provide simple interface for the rest of the app.
 abstract class NetworkRepository<T> extends CrudRepository<T> {
   /// method push data to server overriding data. Used to syncronize local and remote data.
   Future<void> patch(List<T> items);
