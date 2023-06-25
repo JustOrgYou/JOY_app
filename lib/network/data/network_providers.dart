e7d30eb1153000edd5b44dae64f1dc2f9f538fe3
Demezy <38487319+Demezy@users.noreply.github.com>
2023-06-25 12:36:31 +0300

    feat: setup dio

---
+++ lib/network/data/network_providers.dart
@@ -0,0 +1,32 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todo_app/network/data/network_tasks_repository_merger.dart';
+import 'package:todo_app/shared/data/config_providers.dart';
+
+/// Network logic is isolated from the rest of the app. But Dio is freely used
+/// across the network layer.
+final dioProvider = Provider<Dio>(
+  (ref) {
+    final appConfig = ref.watch(appConfigProvider);
+    return Dio(
+      BaseOptions(
+        baseUrl: appConfig.baseUrl,
+        connectTimeout: appConfig.connectTimeout,
+        receiveTimeout: appConfig.receiveTimeout,
+        headers: <String, String>{
+          'Authorization': 'Bearer ${appConfig.authToken}',
+        },
+      ),
+    );
+  },
+);
+
+final networkTasksRepositoryMergerProvider =
+    Provider<NetworkTasksRepositoryMerger>(
+  (ref) {
+    final dio = ref.watch(dioProvider);
+    return NetworkTasksRepositoryMerger(
+      dio: dio,
+    );
+  },
+);
