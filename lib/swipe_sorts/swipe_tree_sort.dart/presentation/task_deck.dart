8ac1f9929178db51a302afa6c9b8083a480f6425
Demezy <38487319+Demezy@users.noreply.github.com>
2023-07-03 13:15:13 +0300

    feat: implement task deck

--- lib/swipe_sorts/swipe_tree_sort.dart/presentation/task_deck.dart
+++ lib/swipe_sorts/swipe_tree_sort.dart/presentation/task_deck.dart
@@ -1,20 +1,37 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:todo_app/swipe_sorts/swipe_tree_sort.dart/presentation/task_deck_card.dart';
+import 'package:todo_app/tasks_service/domain/task_entry.dart';
 
 class TaskDeck extends StatelessWidget {
-  const TaskDeck({Key? key}) : super(key: key);
+  final List<TaskEntry> tasks;
+  const TaskDeck({
+    required this.tasks,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        width: 100,
-        height: 100,
-        color: Colors.amber,
-        child: const Column(
-          children: [
-            Text('TaskDeck'),
-          ],
-        ),
+    return LayoutBuilder(
+      builder: (context, constraints) => Stack(
+        children: [
+          for (final (i, task) in tasks.take(3).indexed.toList().reversed)
+            Align(
+              alignment: Alignment(0, i * 0.05),
+              child: Transform.rotate(
+                angle: pow(-1, i) * pi / 180 * 3 * i,
+                child: SizedBox(
+                  width: constraints.maxWidth / 2,
+                  height: constraints.maxHeight / 6,
+                  child: Opacity(
+                    opacity: 1 - i * 0.2,
+                    child: TaskDeckCard(task: task),
+                  ),
+                ),
+              ),
+            ),
+        ],
       ),
     );
   }
