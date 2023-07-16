84c240d8f810c62c4c941af0ed3612bf9b5db8fe
frog-da <tomilovediana@gmail.com>
2023-07-16 12:33:49 +0300

    Update email_password_login.dart

--- lib/email_password_login/email_password_login.dart
+++ lib/email_password_login/email_password_login.dart
@@ -6,7 +6,6 @@ import 'package:todo_app/email_password_login/presentation/login_password_field.
 import 'package:todo_app/tasks_overview/tasks_overview.dart';
 import 'package:todo_app/generated/l10n.dart';
 
-
 class EmailPasswordLogin extends ConsumerStatefulWidget {
   const EmailPasswordLogin({Key? key}) : super(key: key);
 
@@ -21,17 +20,12 @@ class _EmailPasswordLoginState extends ConsumerState<EmailPasswordLogin> {
 
   Future<void> validateCredentials() async {
     final authService = ref.watch(authServiceProvider);
-    final authenticated = await authService.authenticate(email.text, password.text);
+    final authenticated =
+        await authService.authenticate(email.text, password.text);
     if (!ref.context.mounted) return;
     if (authenticated) {
       // ignore: use_build_context_synchronously
-<<<<<<< Updated upstream
-      Navigator.pop(ref.context);
-      // ignore: use_build_context_synchronously
-      await Navigator.push<void>(
-=======
       await Navigator.pushReplacement<void, void>(
->>>>>>> Stashed changes
         ref.context,
         MaterialPageRoute(
           builder: (context) => const TasksOverview(),
@@ -39,11 +33,7 @@ class _EmailPasswordLoginState extends ConsumerState<EmailPasswordLogin> {
       );
     } else {
       setState(() {
-<<<<<<< Updated upstream
-        errorMessage = 'Invalid email or password.';
-=======
         errorMessage = S.of(context).error_msg;
->>>>>>> Stashed changes
       });
     }
   }
@@ -63,11 +53,7 @@ class _EmailPasswordLoginState extends ConsumerState<EmailPasswordLogin> {
             ),
             ElevatedButton(
               onPressed: validateCredentials,
-<<<<<<< Updated upstream
-              child: const Text('Log In'),
-=======
               child: Text(S.of(context).log_in),
->>>>>>> Stashed changes
             ),
             if (errorMessage.isNotEmpty)
               Text(
